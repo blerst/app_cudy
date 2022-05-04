1f34072636a09b0151e9777b216b1268ae3d2e0a
Rahul <blastyplayz@gmail.com>
2022-05-05 09:34:48 +1000

    Created functional delete button for todos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 //4 May 2022, 14:20
+//Whenever you see <div>, I have commented around them the features about the div because you can't comment in a div.
 
 function App() {
   //This const is creating the hook that will be used for the todos. The useState will give us the ability to create the todo array
@@ -26,6 +27,17 @@ function App() {
     setTodo("")
   }
 
+  function deleteTodo(id) {
+    //The todo id is passed into this function so the delete button knows which todo to remove
+
+    const updateTodo = [...todos].filter((todo) => todo.id !== id) 
+    //Filter operates as an array function (similar to map below)
+
+    setTodos(updateTodo)
+    //This function will update the todo array with the updated version once a function is removed.
+
+  }
+
   return (
     /*
     The form of this div is where the "add todo" option will be
@@ -39,8 +51,16 @@ function App() {
         <input type="text" onChange={(e) => setTodo(e.target.value)} value={todo}/>
         <button type="submit">Add Todo</button>
       </form>
-      {todos.map((todo) => <div>{todo.text}</div>)}
+      {todos.map((todo) => <div key={todo.id}>
+        <div>{todo.text}</div>
+        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+      </div>)}
     </div>
+    //The todos.map allows for elements (like buttons) to be added. For example the remove todo feature
+    //The key inside the map div allows us to ensure that each identifier once on screen is unique.
+    //The onClick handle in the deleteButton allows for a function to execute on click.
+
+
   );
 }
 
