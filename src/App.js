3c0e9ebb8f37186dd9f4ac842c9b785997673b9e
Rahul <blastyplayz@gmail.com>
2022-05-05 10:12:38 +1000

    Created working checkbox for completion

--- src/App.js
+++ src/App.js
@@ -10,10 +10,11 @@ function App() {
   //This const exists as the temp variable for todos that are added before saved in a file.
   const [todo, setTodo] = React.useState("")
 
+  //This function is for adding todos (hence handleSubmit as it handles the user input)
   function handleSubmit(e) {
     //This prevents the webpage from reloading whenever the "Add Todo" button is pushed
     e.preventDefault()
-
+    
     //This is how the todos will be stored
     const newTodo = {
       id: Date.now(),
@@ -27,6 +28,24 @@ function App() {
     setTodo("")
   }
 
+  //The purpose of this function is to toggle variable of whether the todo is completed or not
+  function toggleComplete(id) {
+    const updateTodo = [...todos].map((todo) => {
+      //If the checkbox is changed, change the value to the opposite of what it is (in the variable)
+
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      //While this will return all the todos in the array, it will only modify one
+      return todo
+    })
+
+    setTodos(updateTodo)
+
+  }
+
+
+  //The purpose of this function is to delete todos
   function deleteTodo(id) {
     //The todo id is passed into this function so the delete button knows which todo to remove
 
@@ -54,11 +73,16 @@ function App() {
       {todos.map((todo) => <div key={todo.id}>
         <div>{todo.text}</div>
         <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        <input 
+          type="checkbox" 
+          onChange ={() => toggleComplete(todo.id)}
+          checked={todo.completed} />
       </div>)}
     </div>
     //The todos.map allows for elements (like buttons) to be added. For example the remove todo feature
     //The key inside the map div allows us to ensure that each identifier once on screen is unique.
     //The onClick handle in the deleteButton allows for a function to execute on click.
+    //Checked is a bool value which will change the variable of a todo based on the checked status
 
 
   );
